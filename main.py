507bcd5b044b03d34a437f54a608b151fb1ca253
Joanna Borowiak <joannaborowiak@gmail.com>
2025-07-19 18:04:52 +0200

    Add first frontend page

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, Depends, HTTPException, Header
+from fastapi import FastAPI, Depends, HTTPException, Header, Request, Form
 from sqlalchemy.orm import Session 
 from database import engine, SessionLocal
 from models import Base, User as Userm, Leave
 from schemas import LeaveCreate, Leave as LeaveSchema, UserCreate, User
 import crud
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.requests import Request
+
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -77,3 +84,33 @@ def delete_leave(leave_id: int,
     db.delete(db_leave)
     db.commit()
     return {"detail": "Urlop został usunięty"}
+
+@app.get("/leaves/html", response_class=HTMLResponse)
+def get_leaves_html(request: Request, db: Session = Depends(get_db)):
+    leaves = crud.get_leaves(db)
+    return templates.TemplateResponse("leaves.html", {"request": request, "leaves": leaves})
+
+@app.get("/leaves/form", response_class=HTMLResponse)
+def show_leave_form(request: Request):
+    return templates.TemplateResponse("leave_form.html", {"request": request})
+
+@app.post("/leaves/form")
+def submit_leave_form(
+    request: Request,
+    date_from: str = Form(...),
+    date_to: str = Form(...),
+    comment: str = Form(None),
+    x_user_id: int = Form(...),
+    db: Session = Depends(get_db)
+):
+    new_leave = Leave(
+        user_id=x_user_id,
+        date_from=date_from,
+        date_to=date_to,
+        comment=comment
+    )
+    db.add(new_leave)
+    db.commit()
+    return RedirectResponse(url="/leaves/html", status_code=303)
+
+
